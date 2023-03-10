508a5f4f6e96f47750a24e2caae760187ca1c5b3
Ryan Robbins <ryanarobbins@gmail.com>
2023-03-10 14:25:21 -0500

    setting up FizzBuzz

--- CodeDojo/UnitTest1.cs
+++ CodeDojo/UnitTest1.cs
@@ -2,6 +2,17 @@ namespace CodeDojo
 {
 	public class Tests
 	{
+		/*
+		 * FizzBuzz
+		 * Write a function that takes in an integer and returns FizzBuzz translation of that number
+		 * 1 => 1		6 => Fizz		11 => 11		30 => FizzBuzz		
+		 * 2 => 2		7 => 7			12 => Fizz	
+		 * 3 => Fizz	8 => 8			13 => 13		
+		 * 4 => 4		9 => Fizz		14 => 14	
+		 * 5 => Buzz	10 => Buzz		15 => FizzBuzz		
+		 * 
+		 * 
+		 */
 		[SetUp]
 		public void Setup()
 		{
