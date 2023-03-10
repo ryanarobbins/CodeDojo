a062551d041397317d98441ff55ae20e3b260824
Ryan Robbins <ryanarobbins@gmail.com>
2023-03-10 15:55:47 -0500

    we did some fizzbuzz

--- CodeDojo/UnitTest1.cs
+++ CodeDojo/UnitTest1.cs
@@ -5,23 +5,55 @@ namespace CodeDojo
 		/*
 		 * FizzBuzz
 		 * Write a function that takes in an integer and returns FizzBuzz translation of that number
-		 * 1 => 1		6 => Fizz		11 => 11		30 => FizzBuzz		
-		 * 2 => 2		7 => 7			12 => Fizz	
+		 * 1 => "1"		6 => Fizz		11 => 11		30 => FizzBuzz		
+		 * 2 => "2"		7 => 7			12 => Fizz	
 		 * 3 => Fizz	8 => 8			13 => 13		
 		 * 4 => 4		9 => Fizz		14 => 14	
 		 * 5 => Buzz	10 => Buzz		15 => FizzBuzz		
 		 * 
 		 * 
 		 */
-		[SetUp]
-		public void Setup()
+		
+
+		[Test]
+		[TestCase(1, "1")]
+		[TestCase(6, "Fizz")]
+		public void SixShouldFizz(int toTranslate, string expected)
 		{
+			Assert.That(Translate(toTranslate), Is.EqualTo(expected));
+		}
+        
+
+		[Test]
+		public void TestTrans2 ()
+		{
+			Assert.That(Translate(2), Is.EqualTo("2"));
 		}
 
 		[Test]
-		public void Test1()
+		public void TestTrans15()
 		{
-			Assert.Pass();
+			Assert.That(Translate(15), Is.EqualTo("FizzBuzz"));
 		}
-	}
+
+		[Test]
+		public void FourteenShouldBeFourteen()
+		{
+			Assert.That(Translate(14), Is.EqualTo("14"));
+		}
+
+		private string Translate(int v)
+		{
+
+			return v switch
+			{
+				15 => "FizzBuzz",
+				14 => "14",
+				6 => "Fizz",
+				2 => "2",
+				1 => "1",
+				_ => ""
+			};
+		}
+    }
 }
